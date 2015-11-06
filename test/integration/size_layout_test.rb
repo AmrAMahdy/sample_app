e3e27e6b5ff5e5b02939b20b93e05c9932d5e3f6
Amr Alaa <n1amr1@gmail.com>
2015-11-06 22:06:12 +0200

    Listing 5.38: A direct test the full_title helper.

--- test/integration/size_layout_test.rb
+++ test/integration/size_layout_test.rb
@@ -10,5 +10,8 @@ class SizeLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
